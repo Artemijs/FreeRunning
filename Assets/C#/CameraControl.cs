6395278227a04abb2d54f62792112c52f0ea21b5
Artemijs <artemijspoznaks@gmail.com>
2020-07-08 19:02:05 +0100

    First commit

---
+++ Assets/C#/CameraControl.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+	public GameObject target;
+	private GameObject camera;
+	public float mouseSpeed;
+	public Vector3 cameraDir;
+	//Vector3 _offsetPos;
+	Vector3 _offsetAng;
+	Vector3 _3rdPerspOffset;
+	Vector2 mouseAxis;
+	bool _3rdPerson = false;
+	
+	// Use this for initialization
+	void Start () {
+		camera = gameObject.transform.GetChild (0).gameObject;
+		_3rdPerspOffset = camera.transform.localPosition;
+		_offsetAng = gameObject.transform.localRotation.eulerAngles;
+		//_offsetPos = gameObject.transform.position;
+		mouseAxis = new Vector2(gameObject.transform.localRotation.eulerAngles.x, gameObject.transform.localRotation.eulerAngles.y);
+		cameraDir = GetCamDir ();
+	}
+
+	Vector3 GetCamDir(){
+		Vector3 a = new Vector3 (target.transform.position.x, 0, target.transform.position.z);
+		Vector3 b = new Vector3 (camera.transform.position.x, 0, camera.transform.position.z);
+		return (a - b).normalized;
+	}
+	// Update is called once per frame
+	void Update () {
+		//Vector2 mouseNevv = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+		//Vector2 mouseDelta = mouseOld - mouseNevv;
+		mouseAxis+=new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+		mouseAxis.x += Input.GetAxis ("Mouse X") * mouseSpeed;
+		mouseAxis.y += Input.GetAxis ("Mouse Y") * mouseSpeed;
+		Quaternion lrot = gameObject.transform.localRotation;
+		/*float x = mouseDelta.x * mouseSpeed;
+		mouseDelta.x = mouseDelta.y * mouseSpeed;
+		mouseDelta.y = -x;
+		float mdLen = mouseDelta.magnitude;
+
+		*/
+		/*float mdLen = mouseDelta.magnitude;
+		if (mdLen <= 0.1)
+			return;*/
+		gameObject.transform.position = target.transform.position;
+		cameraDir = GetCamDir ();
+		lrot.eulerAngles = new Vector3 (-mouseAxis.y, mouseAxis.x, 0);//mouseDelta * 0.1f;
+		gameObject.transform.localRotation = lrot;
+		//mouseOld = mouseNevv;
+		if (Input.GetKeyDown(KeyCode.Alpha1)) {
+			Change2FirstPerson();
+		}
+	}
+	public void Change2FirstPerson() {
+		if (_3rdPerson)
+		{
+			camera.transform.localPosition = new Vector3(0, 1, 0);
+			_3rdPerson = false;
+		}
+		else {
+			camera.transform.localPosition = _3rdPerspOffset;
+			_3rdPerson = true;
+		}
+		
+
+	}
+}
---
+++ Assets/C#/PlayerController.cs
@@ -0,0 +1,290 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerController : MonoBehaviour
+{
+	public AnimationCurve _wallRunCurve;
+	Slider _staminaBar;
+	bool[] _input;
+	bool _running;
+	public bool _inAir;
+	public bool _wallRunning;
+	bool _touchingWall;
+	bool _sprinting;
+	bool[] _runningCheck;
+	Transform _ground;
+	Pair<Transform, Vector3> _wallCollision;
+	Rigidbody _body;
+	float _stamina;
+	float _currentStamina;
+	//Vector3 _wallRunDir;
+    // Start is called before the first frame update
+    void Start()
+    {
+		_staminaBar = GameObject.Find("StaminaBar").GetComponent<Slider>();
+		_currentStamina = 1;
+		_stamina = _currentStamina;
+		_sprinting = false;
+		_input = new bool[6];
+		_wallCollision = null;
+		_touchingWall = false;
+		_wallRunning = false;
+		_body = GetComponent<Rigidbody>();
+		//_collidedWithCount = 0;
+		//_runningCheck = new 
+		//if nothing and direction
+		//run
+		//if running and jump
+		//direction jump
+		//if nothing and jump
+		//direction jump
+		//if in air or on ground and direction and action button
+		//wall  run
+		//if wall run and jump
+		//wall jump
+		//if touching wall and jump adn in air
+		//wall jump
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		RecoverStamina();
+		HandleInput();
+		HandleActionCalls();
+		Check4Air();
+		if (_wallRunning) {
+			WallRun();
+		}
+		UpdateStamina();
+	}
+	void RecoverStamina() {
+		if (!_wallRunning && _currentStamina < _stamina) {
+			_currentStamina += Time.deltaTime;
+		}
+	}
+	void HandleActionCalls() {
+		if (IfRunning()) {
+			Running();
+		}
+		else {
+			_running = false;
+		}
+		if (IfSprinting()) {
+			Sprinting();
+			_currentStamina -= Time.deltaTime;
+		}
+		if (IfWallRun()) {
+			StartWallRun();
+		}
+		if (_wallRunning && !_input[5]) {
+			_body.useGravity = true;
+			_wallRunning = false;
+		}
+		if (_wallRunning && !_touchingWall) {
+			_body.useGravity = true;
+			_wallRunning = false;
+		}
+		if (IfDirectionalJump()) {
+			DirectionalJump();
+		}
+		if (IfJump()) {
+			Jump();
+		}
+		//if wallrunning and at corner of wall
+	}
+	void HandleInput() {
+		_input[0] = (Input.GetKey(KeyCode.W));
+		_input[1] = (Input.GetKey(KeyCode.S));
+		_input[2] = (Input.GetKey(KeyCode.A));
+		_input[3] = (Input.GetKey(KeyCode.D));
+
+		_input[4] = (Input.GetKey(KeyCode.Space));
+		_input[5] = (Input.GetKey(KeyCode.LeftShift));
+	}
+	bool IfRunning() {
+		return ((_input[0] || _input[1] || _input[2] || _input[3]) && (!(_input[4]) && !_input[5]) && !_wallRunning);
+	}
+	bool IfSprinting() {
+		return ((_input[0] || _input[1] || _input[2] || _input[3]) && (!_input[4]) && _input[5] && !_wallRunning && !_inAir);
+	}
+	bool IfWallRun() {
+		return ((_input[0] || _input[1] || _input[2] || _input[3]) && !(_input[4]) && _input[5] && _touchingWall && !_wallRunning);
+	}
+	bool IfDirectionalJump() {
+		return ((_input[0] || _input[1] || _input[2] || _input[3]) && _input[4] && !_input[5] && !_inAir);
+	}
+	bool IfJump() {
+		return ((!_input[0] && !_input[1] && !_input[2] && !_input[3]) && _input[4] && !_input[5] && !_inAir);
+	}
+	void WallRun() {
+
+		if (!_touchingWall) {
+			_wallRunning = false;
+			return;
+		}
+		Debug.Log("Wall Run");
+		float speed = 0.15f;
+		float curveValue = 0;
+		_currentStamina -= Time.deltaTime*0.1f;
+		if (_currentStamina <= 0) {
+			OutOfStamina();
+		}
+		Vector3 movDir = GetInputDirection();
+		float dot = Vector3.Dot(movDir, _wallCollision._two);
+		//wallrunDir upwards
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, movDir, out hit, 2)) {
+			//hit.normal;
+			//hit.point;
+			//_wallCollision._two = hit.normal;
+		}
+		//movDir = (Vector3.up + movDir).normalized;
+
+		Vector3 side = Vector3.Cross(_wallCollision._two, Vector3.up);
+		Vector3 newUp = -Vector3.Cross(_wallCollision._two, side);
+		
+		if (Mathf.Abs(dot) > 0.8f) {
+			movDir = newUp;
+		}
+		else {
+			Vector3 cross = Vector3.Cross(_wallCollision._two, Vector3.up);
+			dot = Vector3.Dot(movDir, cross);
+			if (dot > 0) {
+				movDir = cross;
+			}
+			else {
+				movDir = -cross;
+			}
+			curveValue = _wallRunCurve.Evaluate((1 - (_currentStamina / _stamina)));
+			
+			//movDir = (_wallCollision._two + movDir).normalized;
+
+		}
+		_body.velocity = Vector3.zero;
+		transform.position += (movDir + (newUp * curveValue)) * speed;
+		transform.localRotation = Quaternion.LookRotation(movDir);
+	}
+
+	void OutOfStamina() {
+		_currentStamina = 0;
+		_wallRunning = false;
+	}
+	void Check4Air() {
+		
+
+	}
+	void StartWallRun() {
+		Debug.Log("start wall run");
+		_wallRunning = true;
+		_body.useGravity = false;
+	}
+	void Running() {
+		_running = true;
+		float speed = 0.1f;
+		if (_inAir) speed = 0.05f;
+		Vector3 runDir = GetRunningDir();
+		transform.position += runDir * speed;
+		transform.localRotation = Quaternion.LookRotation(runDir);
+
+	}
+	void Sprinting() {
+		Debug.Log("Sprinting");
+		_currentStamina -= Time.deltaTime;
+		_sprinting = true;
+		float speed = 0.15f;
+		Vector3 runDir = GetRunningDir();
+		transform.position += runDir * speed;
+		transform.localRotation = Quaternion.LookRotation(runDir);
+
+	}
+	Vector3 GetInputDirection() {
+		Vector3 forward = Camera.main.transform.forward;
+		forward.y = 0;
+		Vector3 side = Camera.main.transform.right;
+		side.y = 0;
+		Vector3 runFDir = new Vector3();
+		Vector3 runSDir = new Vector3();
+		if (_input[0] || _input[1]) {//if forward backward
+									 //runDir = forward;
+			if (_input[0]) {
+				runFDir = forward;
+			}
+			else runFDir = -forward;
+
+		}
+		if (_input[2] || _input[3]) {//if left right
+			if (_input[2]) {
+				runSDir = -side;
+			}
+			else {
+				runSDir = side;
+			}
+		}
+		return (runSDir + runFDir).normalized;
+		
+	}
+	Vector3 GetRunningDir() {
+
+		Vector3 runDir = GetInputDirection();
+		if (_touchingWall) {
+			float dot = Vector3.Dot(runDir, _wallCollision._two);
+			if(dot > 0) return runDir;
+
+			Vector3 cross = Vector3.Cross(_wallCollision._two, Vector3.up);
+			 dot = Vector3.Dot(runDir, cross);
+			if (dot > 0)
+				runDir = cross;
+			else
+				runDir = -cross;
+		}
+		return runDir;
+	}
+	void DirectionalJump() {
+		_inAir = true;
+
+		_body.AddForce((GetRunningDir() + new Vector3(0, 1, 0))*250);
+	}
+	void Jump() {
+		_inAir = true;
+		transform.position += new Vector3(0, 0.1f, 0);
+		_body.AddForce(Vector3.up * 200);
+	}
+	private void OnCollisionEnter(Collision collision) {
+
+		if (collision.GetContact(0).normal.y > 0.8f) {//if ground
+			_inAir = false;
+			_ground = collision.gameObject.transform;
+		}
+		else { //if not ground
+			   //_collidedNormal = collision.GetContact(0).normal;
+			_wallCollision = new Pair<Transform, Vector3>(collision.transform, collision.GetContact(0).normal);
+			_touchingWall = true;
+		}
+			
+	}
+	private void OnCollisionExit(Collision collision) {
+		if (collision.transform == _ground) {
+			_inAir = true;
+			_ground = null;
+		}
+		else if (_wallCollision._one == collision.transform) {
+			_wallCollision = null;
+			_touchingWall = false;
+		}
+	}
+	public void UpdateStamina() {
+		_staminaBar.value = _currentStamina / _stamina;
+	}
+}
+public class Pair<T, U> {
+	public T _one;
+	public U _two;
+	public Pair() { }
+	public Pair(T one, U two) {
+		_one = one;
+		_two = two;
+	}
+
+}
